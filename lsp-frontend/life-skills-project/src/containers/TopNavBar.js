20842c6b4ca90c0b1fa08e00d7ee9ea8d5ec13ad
BHessel <hesselfitness@gmail.com>
2020-12-28 19:03:06 -0500

    NavBar superskateboard done

--- lsp-frontend/life-skills-project/src/containers/TopNavBar.js
+++ lsp-frontend/life-skills-project/src/containers/TopNavBar.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-
+import '../App.css'
 export default class TopNavBar extends Component {
     render() {
         return (
-            <div>
-                <h1>lets get some life skillz</h1>
+            <div className='Navbar'>
+                <div className='leftSide'>
+                    <h1>Our Logo</h1>
+                </div>
+                <div className='center'>
+                    <input type='text' placeholder='Search...'/>
+                    <button>Search</button></div>
+                <div className='rightSide'>
+                    <h2>Log In/Out</h2>
+                </div>
             </div>
         )
     }
