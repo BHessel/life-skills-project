d7bac79e923c53171fd1de4f49120e1c16c24b77
BHessel <hesselfitness@gmail.com>
2021-01-07 18:30:12 -0500

    did it

--- lsp-frontend/life-skills-project/src/containers/VideoContainer.js
+++ lsp-frontend/life-skills-project/src/containers/VideoContainer.js
@@ -6,16 +6,17 @@ export default class VideoContainer extends Component {
     render() {
         
         return (
-            
             <div className="video-container">
-                    {
-                        
-                           this.props.videos.map((vid, index) =>
-                            (<VideoCard 
+                    <h2 className='video-header'>Ready Learn Some Life Skills?</h2>
+                    <p className='video-subheader'>Your Best Bet for Everything You Didn't Learn in School</p>
+                    {   this.props.videos.map((vid, i) =>
+                        <div className={`vid-${i}`}>
+                            <VideoCard 
                                 video={vid}
-                                key={index}
+                                key={i}
                                 buttonClick={this.props.addToFavorites}
-                            />)
+                            />
+                            </div>
                          )
                         
                     }
